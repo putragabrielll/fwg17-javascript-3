f8921670850d08f01fb8824d6e0956fd8aa165e2
putragabrielll <putragmahk@gmail.com>
2023-10-31 17:08:10 +0700

    tugas 2 terbaru

--- tugas2.js
+++ tugas2.js
@@ -1,6 +1,6 @@
 const getMonth = (callback) => {
     setTimeout(() => {
-        let error = true
+        let error = false
         let month = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
         
         if(!error){
@@ -8,15 +8,21 @@ const getMonth = (callback) => {
         } else {
             callback(new Error('Sorry Data Not Found'), [])
         }
-    }, 0)
+    }, 4000)
 }
 
 const cariBulan = async(a, b) => {
     
     if (a != null){
         console.log(a)
-        
+        return a.message
+    } else {
+        let temp = b.map((e) => {
+            return e
+        })
+        console.log(b)
+        console.log(temp)
+        return temp
     }
-    console.log(b)
 }
 console.log(getMonth(cariBulan))
