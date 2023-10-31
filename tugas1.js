44f8be95e2ab8486b38a330600cba5fa0bf70489
putragabrielll <putragmahk@gmail.com>
2023-10-31 17:10:05 +0700

    update terbaru

--- tugas1.js
+++ tugas1.js
@@ -1,43 +1,43 @@
-const cekHariKerja = (day) => {
-  console.log(day);
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const dataDay = ["senin", "selasa", "rabu", "kamis", "jumat"];
-      let cek = dataDay.find((item) => {
-        return item == day;
-      });
-      if (cek) {
-        resolve(cek);
-        //resolve(cek)
-      } else {
-        reject(new Error("Bukan Hari Kerja"));
-        //reject('Bukan Hari Kerja')
-      }
-    }, 3000);
-  });
-};
-
-let cekHari = async (obj) => {
-  let hasil = await cekHariKerja(obj)
-    .then((result) => {
-      return result;
+const cekHariKerja = (day)=>{
+    console.log(day)
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            const dataDay = ['senin','selasa','rabu','kamis','jumat']
+            let cek = dataDay.find((item)=>{
+                return item == day
+            })
+            if(cek){
+                resolve(cek)
+                //resolve(cek)
+            }else{
+                reject(new Error('Bukan Hari Kerja'))
+                //reject('Bukan Hari Kerja')
+            }
+        },3000)
     })
-    .catch((err) => {
-      return err.message;
-    });
-  console.log(hasil);
-  return hasil;
-};
+}
 
-console.log(cekHari("rabu"));
+let cekHari = async(obj)=>{
+    let hasil = await cekHariKerja(obj).then((result)=>{
+        return result;
+    }).catch((err)=>{
+        return err.message;
+    })
+    console.log(hasil)
+    return hasil;
+}
 
-let hariKerja = async (hari) => {
-  try {
-    let kesimpulan = await cekHariKerja(hari);
-    console.log(kesimpulan);
-  } catch (err) {
-    console.log(err.message);
-  }
-};
+console.log(cekHari('rabu'))
 
-console.log(hariKerja("sabtu"));
+
+
+let hariKerja = async(hari) => {
+    try {
+        let kesimpulan = await cekHariKerja(hari)
+        console.log(kesimpulan)
+    } catch (err){
+        console.log(err.message)
+    }
+}
+
+console.log(hariKerja('sabtu'))
