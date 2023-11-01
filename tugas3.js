7db6252779abef11fe14f6ebf59381766beac2b6
putragabrielll <putragmahk@gmail.com>
2023-11-01 22:09:56 +0700

    fix : tugas 3 terbaru

--- tugas3.js
+++ tugas3.js
@@ -1,21 +1,45 @@
-const data = fetch("https://jsonplaceholder.typicode.com/users");
-
+const panggilData = () => {
+    const data = fetch("https://jsonplaceholder.typicode.com/users");
+    return data
+}
 
 // dengan menggunakan async/await
-const hasil = async() => {
-    const res = await data
-    const getData = await res.json()
-    const dataA = await getData.map(a => a.name)
-    console.log(dataA)
-    return getData
+const hasil = async(x) => {
+    try{
+        const res = await x
+        const getData = await res.json()
+        const dataA = await getData.map(a => a.name)
+        console.log(dataA)
+        return getData
+    }catch(error){
+        console.log(error)
+    }
 }
+hasil(panggilData())
 
 
 // dengan menggunakan then & catch
-hasil().then(res => {
-    const data = res
-    data1 = data.map((a) => {
-        return a.name
+panggilData()
+    .then(result => result.json())
+    .then((res) => {
+        const hasil = res
+        data1 = hasil.map(a => a.name)
+        console.log(data1)
     })
-    console.log(data1)
-})
+    .catch((error) => {
+        console.log(error)
+    })
+
+
+
+
+
+    
+// Tester
+// hasil().then(res => {
+//     const data = res
+//     data1 = data.map((a) => {
+//         return a.name
+//     })
+//     console.log(data1)
+// })
