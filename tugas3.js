b8811dee150430fadab9996deb47c29f7e899316
putragabrielll <putragmahk@gmail.com>
2023-11-01 21:19:21 +0700

    fix : tugas 3 update

--- tugas3.js
+++ tugas3.js
@@ -3,10 +3,17 @@ const data = fetch("https://jsonplaceholder.typicode.com/users");
 const hasil = async() => {
     const res = await data
     const getData = await res.json()
+    console.log(getData)
+
+    const dataA = await getData
+    const dataB = await dataA.map(a => a.name)
+    console.log(dataB)
+    
     return getData
 }
 
-console.log(hasil)
+
+
 hasil().then(res => {
     const data = res
     data1 = data.map((a) => {
